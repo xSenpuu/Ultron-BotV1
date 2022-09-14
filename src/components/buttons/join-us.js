aaf5917f2e04a706b7496dff46ad76dc08590b83
xSenpuu <95657055+xSenpuu@users.noreply.github.com>
2022-09-14 02:13:37 +0100

    Update join-us.js

--- src/components/buttons/join-us.js
+++ src/components/buttons/join-us.js
@@ -1,5 +1,6 @@
 const { Guilds, EmbedBuilder } = require("discord.js");
 const PROSPECT = '960908800787882004'
+const GARRI = '758397862399836260'
 module.exports = {
     data: {
         name: `join-us`
@@ -29,7 +30,8 @@ module.exports = {
               iconURL:
                 "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/907a507d-e421-4e1c-a7f9-a27a30cabd66/df2hcd0-4292b6df-74b3-4eff-a649-3e4b319300c9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkwN2E1MDdkLWU0MjEtNGUxYy1hN2Y5LWEyN2EzMGNhYmQ2NlwvZGYyaGNkMC00MjkyYjZkZi03NGIzLTRlZmYtYTY0OS0zZTRiMzE5MzAwYzkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9hE9xeqK_NrJDA02KEXxPUatldwFFQakkgpD0rSmIag",
               text: "Powered by Stark Industries"
-            })     
+            })  
+                if (member.roles.cache.has(GARRI)){return;} //do nothing for garris
                 if (member.roles.cache.has(PROSPECT)) { // if they already have the role
                     member.roles.remove(PROSPECT); // remove it
                     channel.send({embeds: [leftembed], ephemeral: false})
