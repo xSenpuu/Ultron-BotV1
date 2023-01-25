42f26a06d58e97ff7feb6a32ac076e3beb4d88ad
xSenpuu <95657055+xSenpuu@users.noreply.github.com>
2023-01-25 18:16:16 +0000

    updated join us

--- src/commands/tools/join.js
+++ src/commands/tools/join.js
@@ -34,15 +34,15 @@ module.exports = {
 
         Here's our recruitment process:
         1️⃣ Press the Join GOF 🍆 button below to get Prospecc role.
-        2️⃣ We will be notified if you pick up this role, so don't worry - we'll be nice.
+        2️⃣ You will be DM'd by our Recruiters.
         3️⃣ Jump into games with the members & get on voice.
         4️⃣ We'll let you know after we get some games in with you.
         
         We're always recruiting. Players are usually asked to join the team after a playing with us for a few days in discord voice. It is as simple as joining our voice chat, don't be shy! Even if Competitive isn't for you, we have non competitive members also to play public matches with and seed the server!
         
-        If you want to receive pings for helping us seed the //GOF server, react with the seeder role below 🌱.
+        **If you want to receive pings for helping us seed the //GOF server, react with the seeder role below 🌱.**
         
-        Questions? Contact <@185303412743602176>`
+        Do you have any Questions? Contact <@677859171130933305> regarding Recruitment.`
       )
       .setColor(0x800080)
       .setFooter({
