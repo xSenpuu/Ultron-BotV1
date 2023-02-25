8dbfc5434ae1b381312bd35264b51108c902d7f2
Nathan Hierl <nshierl@mtu.edu>
2023-02-25 15:20:44 -0500

    seeder ai message good for deploy

--- src/commands/tools/seeder_ping_bug_fact.js
+++ src/commands/tools/seeder_ping_bug_fact.js
@@ -17,15 +17,23 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("ultron_seed_w_bug_fact")
         .setDescription("Ping Seeders with a random bug fact")
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+        .setDefaultMemberPermissions(PermissionFlagsBits.UseApplicationCommands),
     async execute(interaction, client) {
 
+        if (!interaction.member.roles.cache.has(process.env.SEEDER_PING_R)) {
+            await interaction.reply({
+                content: "You are not allowed to use this command, sorry...",
+                ephemeral: true
+            });
+
+            return
+        }
         const channelID = process.env.SEEDER_C //#seeder
         const roleID = process.env.SEEDER_R //@seeder
         const channel = interaction.guild.channels.cache.get(channelID)
         const role = interaction.guild.roles.cache.get(roleID)
 
-        await interaction.deferReply();
+        await interaction.deferReply({ ephemeral: true });
 
         const fact_prompt = `Give me a random bug or insect fact`
 
