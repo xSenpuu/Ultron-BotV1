402d8912416f0a6bf79d2753c4bd2dfa4dcbc534
Nathan Hierl <nshierl@mtu.edu>
2023-03-07 17:16:00 -0500

    disabled gain garri role for now, combined seeder story and bug facts

--- src/commands/tools/seeder_ping.js
+++ src/commands/tools/seeder_ping.js
@@ -15,8 +15,8 @@ const openai = new OpenAIApi(configuration);
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("ultron_seed_w_story")
-        .setDescription("Ping Seeders with a Story about a Random Member")
+        .setName("ultron_seed")
+        .setDescription("Ping Seeders with a Story about a Random Seeder and a bug fact for the plebs")
         .setDefaultMemberPermissions(PermissionFlagsBits.UseApplicationCommands),
     async execute(interaction, client) {
 
@@ -36,9 +36,9 @@ module.exports = {
 
         await interaction.deferReply({ ephemeral: true });
 
-        let garriRole = await interaction.guild.roles.fetch(process.env.GARRI_R)
+        let garriRole = await interaction.guild.roles.fetch(process.env.SEEDER_R)
         let members = await interaction.guild.members.fetch()
-        let garris = members.filter(m => m._roles.includes(process.env.GARRI_R))
+        let garris = members.filter(m => m._roles.includes(process.env.SEEDER_R))
 
         garri = garris.get(Array.from(garris.keys())[Math.floor(Math.random() * garris.size)])
 
@@ -57,7 +57,21 @@ module.exports = {
 
         response_text = `${role} Let me tell you a story about ${garri.displayName}...` +
             story.data.choices[0].text +
-            "\n\nMoral of the Story: Seed GOF1!";
+            "\n\nMoral of the Story: Seed GOF1!\n\n";
+
+        const fact_prompt = `Give me a random bug or insect fact`
+
+        const fact = await openai.createCompletion({
+            model: process.env.OPENAI_MODEL,
+            prompt: fact_prompt,
+            temperature: parseFloat(process.env.OPENAI_TEMPERATURE),
+            max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS),
+        });
+
+        response_text = response_text +
+            "***********************************************************\n\n" +
+            "And now, a probably accurate bug fact for you buggy people!" +
+            fact.data.choices[0].text;
 
         await channel.send(response_text)
         await interaction.editReply({ content: "Ping successfully sent to seeder channel", ephemeral: true })
