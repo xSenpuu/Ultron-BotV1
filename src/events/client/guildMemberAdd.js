53838cc19c30724cb9a24aad9e5203636b6b4e1e
xSenpuu <95657055+xSenpuu@users.noreply.github.com>
2023-03-11 09:26:26 +0000

    Update guildMemberAdd.js

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -28,7 +28,7 @@ module.exports = {
         .setFooter({
         iconURL:
           "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/907a507d-e421-4e1c-a7f9-a27a30cabd66/df2hcd0-4292b6df-74b3-4eff-a649-3e4b319300c9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkwN2E1MDdkLWU0MjEtNGUxYy1hN2Y5LWEyN2EzMGNhYmQ2NlwvZGYyaGNkMC00MjkyYjZkZi03NGIzLTRlZmYtYTY0OS0zZTRiMzE5MzAwYzkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9hE9xeqK_NrJDA02KEXxPUatldwFFQakkgpD0rSmIag",
-        text: "Muck is a cuck."
+        text: "Powered by Stark Industries"
       })
 
         const channel = interaction.guild.channels.cache.get(channelID)
